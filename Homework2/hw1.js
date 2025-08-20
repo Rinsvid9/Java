bcf53dfa97daaeae5a5b6a7a0f854caff22279ad
«rinsvid» <rinsvid9@gmail.com>
2025-08-20 07:06:49 +0300

    ікс .gitignore для домашніх

---
+++ Homework2/hw1.js
@@ -0,0 +1,156 @@
+// let list = [1, 3, 4, 5, "Rinsvid", 'fire', 56, -95, 100];
+//
+// console.log(list);
+// console.log(list[0]);
+// console.log(list[1]);
+// console.log(list[2]);
+// console.log(list[3]);
+// console.log(list[4]);
+// console.log(list[5]);
+// console.log(list[6]);
+// console.log(list[7]);
+// console.log(list[8]);
+// console.log(list[9]);
+//
+// let colorof = {
+//     title: "Color of magic",
+//     pageCount: 288,
+//     genre: "Fantasy"
+// }
+//
+// let theLight = {
+//     title: "The Light Fantastic",
+//     pageCount: 285,
+//     genre: "Fantasy"
+// }
+//
+// let equal = {
+//     title: "Equal Rites",
+//     pageCount: 282,
+//     genre: "Fantasy"
+// }
+//
+// let mort = {
+//     title: "Mort",
+//     pageCount: 304,
+//     genre: "Fantasy",
+//     authors: [
+//         {name: "Terry Pratchett", age: 66}
+//     ]
+// }
+//
+// let sour = {
+//     title: "Sourcery",
+//     pageCount: 336,
+//     genre: "Fantasy",
+//     authors: [
+//         {name: "Terry Pratchett", age: 66}
+//     ]
+// }
+//
+// let wyrd = {
+//     title: "Wyrd Sisters",
+//     pageCount: 368,
+//     genre: "Fantasy",
+//     authors: [
+//         {name: "Terry Pratchett", age: 66}
+//     ]
+// }
+
+// let users = [
+//     {name: "rfkjdkjr", username: 'kjrflr', password: "48765jfgr"},
+//     {name: "kjrkjfdkj", username: 'klkj', password: "y95yrfhb"},
+//     {name: "kjfvkjs", username: 'mnen', password: "987598hf"},
+//     {name: "nkk", username: 'neflwl', password: "gt565t"},
+//     {name: "jnjs", username: 'krhfkjhkjr', password: "5ty9yr5jn"},
+//     {name: "lkeke", username: 'nfkjhsk', password: "87r987r"},
+//     {name: "dlke;l", username: 'kjkvuhsk', password: "8798yuengkj"},
+//     {name: "wkeoj", username: 'ekhh', password: "7987hrgjkr"},
+//     {name: "elk;k", username: 'fsuihfe', password: "879rhgkjnriy7"},
+//     {name: "deoke", username: 'kjhdf', password: "7987jn"}
+// ]
+// let mypas0 = users[0]
+// let mypas1 = users[1]
+// let mypas2 = users[2]
+// let mypas3 = users[3]
+// let mypas4 = users[4]
+// let mypas5 = users[5]
+// let mypas6 = users[6]
+// let mypas7 = users[7]
+// let mypas8 = users[8]
+// let mypas9 = users[9]
+// console.log(mypas9);
+
+
+// let weather = {
+//     morning:{
+//         monday: +18,
+//         tuesday: +18,
+//         wednesday: +16,
+//         thursday: +18,
+//         friday: +21,
+//         saturday: +18,
+//         sunday: +20,
+//     },
+//     afternoon:{
+//         monday: +27,
+//         tuesday: +25,
+//         wednesday: +30,
+//         thursday: +28,
+//         friday: +30,
+//         saturday: +35,
+//         sunday: +28,
+//     },
+//     evening:{
+//         monday: +24,
+//         tuesday: +23,
+//         wednesday: +25,
+//         thursday: +26,
+//         friday: +25,
+//         saturday: +27,
+//         sunday: +24,
+//     }
+// }
+// console.log(weather);
+//
+// let weather2 = {
+//     monday: {
+//         morning: +18,
+//         afternoon: +27,
+//         evening: +24,
+//     },
+//     tuesday: {
+//         morning: +18,
+//         afternoon: +25,
+//         evening: +23,
+//     },
+//     wednesday: {
+//         morning: +16,
+//         afternoon: +30,
+//         evening: +25,
+//     },
+//     thursday: {
+//         morning: +18,
+//         afternoon: +28,
+//         evening: +26,
+//     },
+//     friday: {
+//         morning: +21,
+//         afternoon: +30,
+//         evening: +25,
+//     },
+//     saturday: {
+//         morning: +18,
+//         afternoon: +35,
+//         evening: +27,
+//     },
+//     sunday: {
+//         morning: +18,
+//         afternoon: +28,
+//         evening: +24,
+//     }
+//
+// }
+//
+// console.log(weather2);
+
